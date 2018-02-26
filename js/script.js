ec6e5760e3b491ff7daa8041f873f63232e5f7a1
Ebrahem Samer <Soltan_Algaram41@yahoo.com>
2018-02-26 16:41:16 +0200

    Editing project modal and fixing files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
+// working with search input 
 $(".search-input input").click(function() {
 		$(".search-input input").css("transition" , "2s");
 		$(".search-input input").css("opacity" , 1);
 });
+
+// working with modal 
+
+function cssChange(selector , css) {
+	$(selector).css(css);
+}
+
+
+$(".sign-up-span").click(function() {
+	cssChange(".sign-up-span" , {"color" : "green" , "opacity" : "1"});
+	cssChange(".login-span" , {"color" : "black" , "opacity" : ".5"});
+	cssChange("#login-modal" , {"display" : "none"});
+	cssChange("#sign-up-modal" , {"display" : "block"});
+});
+$(".login-span").click(function() {
+	cssChange(".login-span" , {"color" : "green" ,"opacity" : "1"});
+	cssChange(".sign-up-span" ,{"color" : "black" , "opacity" : ".5"});
+	cssChange("#sign-up-modal" , {"display" : "none"});
+	cssChange("#login-modal" , {"display" : "block"});
+});
+
+// jquery playing 
 
